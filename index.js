95472249410b292f89cae7dc0bd09e709829472b
Asif Kabir Emon <asif.kabir@coolriots.io>
2023-11-09 10:09:55 +0800

    change filter

--- index.js
+++ index.js
@@ -42,80 +42,56 @@ const run = async () => {
             const target = req.body.target;
             const action = req.body.action;
             if (action === 'search') {
-                const parameters = req.body.parameters;
-                const searchWay = req.body.searchWay;
-                const columnOrder = req.body.columnOrder;
-                const page = req.body.page;
-                const limit = req.body.maxRow;
+                // const parameters = req.body.parameters;
+                const filter = req.body.filter;
+                const columnOrder = req.body.columnsToRetrieve;
+                const page = req.body.pageToRetrieve;
+                const limit = req.body.maxRowsPerPage;
 
                 const targetCollection = database.collection(target);
 
                 const project = {};
 
                 columnOrder.forEach(element => {
-                    project[element] = 1;
+                    project[element.key] = 1;
                 });
                 console.log(project);
 
-                if (Object.keys(parameters).length === 0) {
-                    const totalDocument = await targetCollection.countDocuments();
-                    const cursor = await targetCollection.aggregate([
-                        {
-                            $skip: page * limit
-                        },
-                        {
-                            $limit: limit
-                        },
-                        {
-                            $project: project
-                        }
-                    ]).toArray();
-                    return res.status(200).send({
-                        target: target,
-                        action: action,
-                        tableCol: columnOrder,
-                        tableRow: cursor,
-                        totalPage: Math.ceil(totalDocument / limit),
-                    });
-                }
-
-                console.log(searchWay);
                 const match = {};
-                if (searchWay.condition === 'or' || searchWay.condition === 'and') {
+                if (filter.logicalOperator === 'or' || filter.logicalOperator === 'and') {
                     const matchFields = [];
-                    searchWay.fields.forEach(({ key, dataType, operator }) => {
-                        if (parameters[key]) {
+                    filter.rules.forEach(({ field, dataType, comparisonOperator, value }) => {
                             if (dataType === 'string') {
-                                if (operator === "match") {
-                                    matchFields.push({ [key]: { $regex: parameters[key], $options: 'i' } });
-                                } else if (operator === "equal") {
-                                    matchFields.push({ [key]: parameters[key] });
+                                if (comparisonOperator === "match") {
+                                    matchFields.push({ [field]: { $regex: value, $options: 'i' } });
+                                } else if (comparisonOperator === "equal") {
+                                    matchFields.push({ [field]: value });
                                 }
                             }
                             else if (dataType === 'number') {
-                                if (operator === "equal") {
-                                    matchFields.push({ [key]: parameters[key] });
-                                } else if (operator === "greaterThan") {
-                                    matchFields.push({ [key]: { $gt: Number(parameters[key]) } });
-                                } else if (operator === "lessThan") {
-                                    matchFields.push({ [key]: { $lt: Number(parameters[key]) } });
-                                } else if (operator === "greaterThanOrEqual") {
-                                    matchFields.push({ [key]: { $gte: Number(parameters[key]) } });
-                                } else if (operator === "lessThanOrEqual") {
-                                    matchFields.push({ [key]: { $lte: Number(parameters[key]) } });
-                                } else if (operator === "notEqual") {
-                                    matchFields.push({ [key]: { $ne: Number(parameters[key]) } });
+                                if (comparisonOperator === "equal") {
+                                    matchFields.push({ [field]: value });
+                                } else if (comparisonOperator === "greaterThan") {
+                                    matchFields.push({ [field]: { $gt: Number(value) } });
+                                } else if (comparisonOperator === "lessThan") {
+                                    matchFields.push({ [field]: { $lt: Number(value) } });
+                                } else if (comparisonOperator === "greaterThanOrEqual") {
+                                    matchFields.push({ [field]: { $gte: Number(value) } });
+                                } else if (comparisonOperator === "lessThanOrEqual") {
+                                    matchFields.push({ [field]: { $lte: Number(value) } });
+                                } else if (comparisonOperator === "notEqual") {
+                                    matchFields.push({ [field]: { $ne: Number(value) } });
                                 }
                             }
                             else if (dataType === 'date') {
-                                matchFields.push({ [key]: parameters[key] });
+                                matchFields.push({ [field]: value });
                             }
                         }
-                    });
-                    if (searchWay.condition === 'or') {
+                    );
+                    if (filter.logicalOperator === 'or') {
                         match['$or'] = [...matchFields];
                     }
-                    else if (searchWay.condition === 'and') {
+                    else if (filter.logicalOperator === 'and') {
                         match['$and'] = [...matchFields];
                     }
                 }
@@ -148,6 +124,118 @@ const run = async () => {
                 });
             }
         });
+
+        // // Query for getting user data
+        // app.post('/api/tableData', async (req, res) => {
+        //     const target = req.body.target;
+        //     const action = req.body.action;
+        //     if (action === 'search') {
+        //         const parameters = req.body.parameters;
+        //         const searchWay = req.body.searchWay;
+        //         const columnOrder = req.body.columnOrder;
+        //         const page = req.body.page;
+        //         const limit = req.body.maxRow;
+
+        //         const targetCollection = database.collection(target);
+
+        //         const project = {};
+
+        //         columnOrder.forEach(element => {
+        //             project[element] = 1;
+        //         });
+        //         console.log(project);
+
+        //         if (Object.keys(parameters).length === 0) {
+        //             const totalDocument = await targetCollection.countDocuments();
+        //             const cursor = await targetCollection.aggregate([
+        //                 {
+        //                     $skip: page * limit
+        //                 },
+        //                 {
+        //                     $limit: limit
+        //                 },
+        //                 {
+        //                     $project: project
+        //                 }
+        //             ]).toArray();
+        //             return res.status(200).send({
+        //                 target: target,
+        //                 action: action,
+        //                 tableCol: columnOrder,
+        //                 tableRow: cursor,
+        //                 totalPage: Math.ceil(totalDocument / limit),
+        //             });
+        //         }
+
+        //         console.log(searchWay);
+        //         const match = {};
+        //         if (searchWay.condition === 'or' || searchWay.condition === 'and') {
+        //             const matchFields = [];
+        //             searchWay.fields.forEach(({ key, dataType, operator }) => {
+        //                 if (parameters[key]) {
+        //                     if (dataType === 'string') {
+        //                         if (operator === "match") {
+        //                             matchFields.push({ [key]: { $regex: parameters[key], $options: 'i' } });
+        //                         } else if (operator === "equal") {
+        //                             matchFields.push({ [key]: parameters[key] });
+        //                         }
+        //                     }
+        //                     else if (dataType === 'number') {
+        //                         if (operator === "equal") {
+        //                             matchFields.push({ [key]: parameters[key] });
+        //                         } else if (operator === "greaterThan") {
+        //                             matchFields.push({ [key]: { $gt: Number(parameters[key]) } });
+        //                         } else if (operator === "lessThan") {
+        //                             matchFields.push({ [key]: { $lt: Number(parameters[key]) } });
+        //                         } else if (operator === "greaterThanOrEqual") {
+        //                             matchFields.push({ [key]: { $gte: Number(parameters[key]) } });
+        //                         } else if (operator === "lessThanOrEqual") {
+        //                             matchFields.push({ [key]: { $lte: Number(parameters[key]) } });
+        //                         } else if (operator === "notEqual") {
+        //                             matchFields.push({ [key]: { $ne: Number(parameters[key]) } });
+        //                         }
+        //                     }
+        //                     else if (dataType === 'date') {
+        //                         matchFields.push({ [key]: parameters[key] });
+        //                     }
+        //                 }
+        //             });
+        //             if (searchWay.condition === 'or') {
+        //                 match['$or'] = [...matchFields];
+        //             }
+        //             else if (searchWay.condition === 'and') {
+        //                 match['$and'] = [...matchFields];
+        //             }
+        //         }
+        //         console.log(match);
+
+        //         const totalDocument = await targetCollection.countDocuments(match);
+
+        //         const cursor = await targetCollection.aggregate([
+        //             {
+        //                 $match: match
+        //             },
+        //             {
+        //                 $skip: page * limit
+        //             },
+        //             {
+        //                 $limit: limit
+        //             },
+        //             {
+        //                 $project: project
+        //             }
+        //         ]).toArray();
+        //         console.log(cursor);
+                
+        //         res.status(200).send({
+        //             target: target,
+        //             action: action,
+        //             tableCol: columnOrder,
+        //             tableRow: cursor,
+        //             totalPage: Math.ceil(totalDocument / limit),
+        //         });
+        //     }
+        // });
 
         // Query for getting all countries
         app.get("/api/countries", async (req, res) => {
