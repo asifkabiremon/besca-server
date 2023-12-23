3769701443896dd3e999c4c9a377ba8552b9e298
Md Asif Kabir Emon <139751515+asifkabiremon@users.noreply.github.com>
2023-12-23 10:20:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ exports.app = app;
 
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
