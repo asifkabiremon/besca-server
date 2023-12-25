8d163497b6439308acb9d92a5aa8be1b5ffd1b16
Md Asif Kabir Emon <139751515+asifkabiremon@users.noreply.github.com>
2023-12-25 16:25:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,12 +44,7 @@ const run = async () => {
         app.get("/api/template/:templateName", async (req, res) => {
             const templateName = req.params.templateName;
             const cursor = await templateCollection.findOne({ templateName: templateName });
-            if (cursor) {
-                res.status(200).send(cursor);
-            }
-            else {
-                res.status(404).send("No data found!");
-            }
+            res.send(cursor);
         });
 
         // Query for getting user data
